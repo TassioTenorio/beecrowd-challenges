b7b691a0a107a2627e0fce24270f4245d18174b3
TassioTenorio <121881174+TassioTenorio@users.noreply.github.com>
2023-09-25 18:01:11 -0300

    adding modifications in 1001.py

--- 1001.py
+++ 1001.py
@@ -1,9 +1,9 @@
-# A = float(input())
-# B = float(input())
-# C = float(input())
+A = float(input())
+B = float(input())
+C = float(input())
 
-# MEDIA = ((A*2)+(B*3)+(C*5))/10
-# print("MEDIA = ", round(MEDIA, 1))
+MEDIA = ((A*2)+(B*3)+(C*5))/10
+print("MEDIA = ", round(MEDIA, 1))
 
 # A = int(input())
 # B = int(input())
